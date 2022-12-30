b1671ed421a730ec9f9f2f833ea5d390d64cf61f
ussef11 <69126473+ussef11@users.noreply.github.com>
2022-12-30 10:19:10 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
+app.all('/tesApi', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 5000)
